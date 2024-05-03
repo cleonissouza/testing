c8b57b8aca0b43706b260761b2369a0a1a653136
cleonissouza <cleonis-cls@outlook.com>
2024-05-03 13:04:43 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let poteCafe = "cafe pilao " 
+let poteAcucar = "acucar cristal " 
+let poteBiscoito = "biscoito maizena "
+const mensagemDaVovo = "Hoje na cozinha da vovo tem "
+console.log(mensagemDaVovo + poteCafe + ", " + poteAcucar  + ", " + poteBiscoito)
